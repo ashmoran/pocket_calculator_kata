02deabb4604ee795187f0417b2a504b66046e408
Ash Moran <ash.moran@patchspace.co.uk>
2013-01-05 22:17:11 +0000

    AC

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,9 @@
 class Calculator
+  def initialize(dependencies)
+    @display = dependencies.fetch(:display)
+  end
 
+  def ac
+    @display.update("0")
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,7 +1,23 @@
 require 'calculator'
 
 describe Calculator do
-  it "does something" do
-    pending
+  subject(:calculator) { Calculator.new(display: self) }
+
+  def update(contents)
+    @displayed_values << contents
+  end
+
+  def display_contents
+    @displayed_values.last
+  end
+
+  before(:each) do
+    @displayed_values = [ ]
+  end
+
+  describe "AC" do
+    specify {
+      expect { calculator.ac }.to change { display_contents }.from(nil).to("0")
+    }
   end
 end
