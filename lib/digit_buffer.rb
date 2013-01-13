3bccbcc1f53c892b77cf9fa3e72c1b8c684dbbb0
Ash Moran <ash.moran@patchspace.co.uk>
2013-01-13 12:47:18 +0000

    Extract DigitBuffer

---
+++ lib/digit_buffer.rb
@@ -0,0 +1,37 @@
+class DigitBuffer
+  def clear
+    @digits = [ ]
+  end
+
+  def add_digit(digit)
+    @digits << digit if @digits.length < 10
+  end
+
+  def delete_digit
+    @digits.pop
+  end
+
+  def toggle_sign
+    if @digits.first == "-"
+      @digits.shift
+    else
+      @digits.unshift("-")
+    end
+  end
+
+  def read_in_number(number)
+    @digits = number.to_s.chars.to_a
+  end
+
+  def to_number
+    @digits.join.to_i
+  end
+
+  def to_s
+    to_number.to_s
+  end
+
+  def join
+    @digits.join
+  end
+end
