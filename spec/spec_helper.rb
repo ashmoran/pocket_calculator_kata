47060f7f85343cd879b8b62fb538ea5d5410f909
Ash Moran <ash.moran@patchspace.co.uk>
2013-01-14 16:55:06 +0000

    Simplify the typing helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,26 +14,23 @@ module PocketCalculatorHelper
     :m_minus    => :m_minus,
     :mr         => :mr,
     :mc         => :mc,
-    :"0"        => :n0,
-    :"1"        => :n1,
-    :"2"        => :n2,
-    :"3"        => :n3,
-    :"4"        => :n4,
-    :"5"        => :n5,
-    :"6"        => :n6,
-    :"7"        => :n7,
-    :"8"        => :n8,
-    :"9"        => :n9,
+    0           => :n0,
+    1           => :n1,
+    2           => :n2,
+    3           => :n3,
+    4           => :n4,
+    5           => :n5,
+    6           => :n6,
+    7           => :n7,
+    8           => :n8,
+    9           => :n9,
     :point      => :point,
     :backspace  => :backspace
   }
 
   def press(*buttons)
     buttons.each do |button|
-      button_symbol = button.to_s.to_sym
-      if BUTTON_METHODS.has_key?(button_symbol)
-        subject.send(BUTTON_METHODS[button_symbol])
-      end
+      subject.send(BUTTON_METHODS.fetch(button))
     end
   end
 end
